3e34e74428c4ccad83dc99b99ddc52a84aee0493
George_Chakama <gtchakama@gmail.com>
2021-06-23 14:12:33 +0200

    Header toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav } from "react-bootstrap";
 
-export default function Header() {
+
+// import './Header.css';
+const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
+
+        <Navbar collapseOnSelect expand="lg" style={{ background: 'green', padding: 10, fontSize: 20 }} variant="dark">
+            <Navbar.Brand href="/">Travel Guide</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
+                {/* <Nav>
+                    <Nav.Link href="#deets">Login</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                        Register
+            </Nav.Link>
+                </Nav> */}
+            </Navbar.Collapse>
+        </Navbar>
+
+    );
 }
+
+export default Header;
