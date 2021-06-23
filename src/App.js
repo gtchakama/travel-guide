6d0f5e217688ce4bfae8c9df21158a8d0d63bb4d
George_Chakama <gtchakama@gmail.com>
2021-06-23 13:45:53 +0200

    Axios req

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "./components/Home";
+import Header from './components/Header'
+import About from "./components/About";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Header />
+      <Switch>
+
+        <Route exact path='/about' component={About} />
+        <Route exact path='/' component={Home} />
+
+      </Switch>
+      <Footer />
+      
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import CountriesList from "./CountriesList";
+import SearchForm from './SearchForm';
+import axios from "axios";
+
+
+const Home = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [country, setCountry] = useState([])
+    const [searchTerm, SetSearchTerm] = useState('ghana');
+
+    useEffect(() => {
+        setIsLoading(true)
+        const getCountries = async () => {
+            try {
+                const response = await axios.get(`https://restcountries.eu/rest/v2/name/${searchTerm}?fullText=true`)
+
+                setCountry(response.data)
+                setIsLoading(false)
+
+            } catch (error) {
+                setIsLoading(false)
+            }
+        }
+
+
+        getCountries()
+    }, [searchTerm])
+    console.log(country)
+    return (
+        <div>
+            <SearchForm SetSearchTerm={SetSearchTerm} />
+            <CountriesList country={country} isLoading={isLoading} />
+        </div>
+    );
+}
+
+export default Home;
